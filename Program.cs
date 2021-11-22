c5a24f87d33a7a94d84200433a03a50808375d01
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-11-22 13:02:13 +0700

    make simple process and show user's display name

--- Program.cs
+++ Program.cs
@@ -2,13 +2,16 @@
 using Microsoft.Extensions.Configuration;
 using simple_graph_console;
 
-Console.WriteLine("Hello, World!");
 
-var appConfig = LoadAppSettings();
 
-if (appConfig == null)
+var appConfig = new ConfigurationBuilder()
+        .AddUserSecrets<Program>()
+        .Build();
+
+if (string.IsNullOrEmpty(appConfig["appId"]) ||
+    string.IsNullOrEmpty(appConfig["scopes"]))
 {
-    Console.WriteLine("Missing or invalid appsettings.json...exiting");
+    Console.WriteLine("Missing or invalid appsettings.json...exiting\n");
     return;
 }
 
@@ -16,15 +19,15 @@ var appId = appConfig["appId"];
 var scopesString = appConfig["scopes"];
 var scopes = scopesString.Split(';');
 
-// Initialize Graph client
-GraphHelper.Initialize(appId, scopes, (code, cancellation) => {
+GraphHelper.Initialize(appId, scopes, (code, cancellation) =>
+{
     Console.WriteLine(code.Message);
     return Task.FromResult(0);
 });
 
 var accessToken = GraphHelper.GetAccessTokenAsync(scopes).Result;
+Console.WriteLine("Signed In...\n");
 Console.WriteLine($"Access token: {accessToken}\n");
-
 int choice = -1;
 
 while (choice != 0)
@@ -32,9 +35,7 @@ while (choice != 0)
     Console.WriteLine("Please choose one of the following options:");
     Console.WriteLine("0. Exit");
     Console.WriteLine("1. Display access token");
-    Console.WriteLine("2. View this week's calendar");
-    Console.WriteLine("3. Add an event");
-    Console.WriteLine("4. Say, Hi");
+    Console.WriteLine("2. Hello, Me.");
 
     try
     {
@@ -42,47 +43,23 @@ while (choice != 0)
     }
     catch (System.FormatException)
     {
-        // Set to invalid value
         choice = -1;
     }
 
     switch (choice)
     {
         case 0:
-            // Exit the program
             Console.WriteLine("Goodbye...");
             break;
         case 1:
-            // Display access token
+            Console.WriteLine($"Access token: {accessToken}\n");
             break;
         case 2:
-            // List the calendar
-            break;
-        case 3:
-            // Create a new event
-            break;
-        case 4: 
-            var user = GraphHelper.GetMeAsync().Result;
-            Console.WriteLine($"Hi, {user.DisplayName}");
+            var user = await GraphHelper.GetMeAsync();
+            Console.WriteLine($"Hi, {user.DisplayName}\n");
             break;
         default:
             Console.WriteLine("Invalid choice! Please try again.");
             break;
     }
-}
-
-static IConfigurationRoot LoadAppSettings()
-{
-    var appConfig = new ConfigurationBuilder()
-        .AddUserSecrets<Program>()
-        .Build();
-
-    // Check for required settings
-    if (string.IsNullOrEmpty(appConfig["appId"]) ||
-        string.IsNullOrEmpty(appConfig["scopes"]))
-    {
-        return null;
-    }
-
-    return appConfig;
 }
